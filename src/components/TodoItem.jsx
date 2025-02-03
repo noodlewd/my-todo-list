e707056ccc018d2927a6cf6d165c62c3f5881739
noodlewd <michel35@naver.com>
2025-02-03 20:08:18 +0900

    feat : 최종본

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const TodoItem = ({ data, index, handleWorkDelete, handleComplete }) => {
   return (
     <div className="work-form">
-      <span className="work-title">제목{data.title}</span>
+      <span className="work-title">{data.title}</span>
       <br />
-      <span className="work-content">내용{data.content}</span>
+      <span className="work-content">{data.content}</span>
       <br />
       <button className="work-delete-btn" onClick={() => handleWorkDelete(index)}>
         삭제하기
